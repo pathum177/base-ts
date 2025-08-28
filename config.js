9f904b3fec561ee92a14ad217cdaabb7c12f9e78
pathum177 <pathummalsara177@gmail.com>
2025-08-28 12:00:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=VIIxyRYC#gnTjHnJnOd1COuDlxoBs8xCii2yM7TzPNbi4TYpBiS8", // session id
+SESSION_ID: process.env.SESSION_ID || "KSMD~2oQ0GJQI#FsUeb8W2XqDNmPLBGHEl72Nbp1jibY5ea-ssNJPSJrI", // session id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
 ANTI_DELETE: process.env.ANTI_DELETE || "true", // true,false
 MODE: process.env.MODE || "public", // public,private,groups,inbox
